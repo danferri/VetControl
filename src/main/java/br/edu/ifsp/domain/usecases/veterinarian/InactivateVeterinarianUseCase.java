cc43174200e086c4c38d88da60bdff010b7f1b71
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-06-01 11:24:06 -0300

    feat: Implement Deactive Veterinarian Use case and Veterianarian Service

--- src/main/java/br/edu/ifsp/domain/usecases/veterinarian/InactivateVeterinarianUseCase.java
+++ src/main/java/br/edu/ifsp/domain/usecases/veterinarian/InactivateVeterinarianUseCase.java
@@ -1,6 +1,24 @@
 package br.edu.ifsp.domain.usecases.veterinarian;
 
-//CDU005
+import br.edu.ifsp.domain.model.user.Veterinarian;
+import br.edu.ifsp.domain.model.user.VeterinarianRepository;
+import br.edu.ifsp.domain.model.user.VeterinarianStatus;
 
-public class InactivateVeterinarianUseCase {
+public class DeactivateVeterinarianUseCase {
+    private VeterinarianRepository veterinarianRepository;
+
+    public DeactivateVeterinarianUseCase(VeterinarianRepository veterinarianRepository) {
+        this.veterinarianRepository = veterinarianRepository;
+    }
+
+    public void inativarVeterinario(String crmv) {
+        Veterinarian veterinarian = veterinarianRepository.findByCrmv(crmv);
+        if (veterinarian != null) {
+            VeterinarianStatus deactivated = VeterinarianStatus.INACTIVE;
+            veterinarian.setStatus(deactivated);
+            veterinarianRepository.update(veterinarian);
+        } else {
+            throw new IllegalArgumentException("Veterinário não encontrado com o CRMV fornecido.");
+        }
+    }
 }
