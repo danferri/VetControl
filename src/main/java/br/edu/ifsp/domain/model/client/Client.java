b6e5ef1daef7e11a9e7498a771ab14d9d16e536b
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-06-01 12:48:32 -0300

    feat: add List of Pets in client

--- src/main/java/br/edu/ifsp/domain/model/client/Client.java
+++ src/main/java/br/edu/ifsp/domain/model/client/Client.java
@@ -1,33 +1,56 @@
 package br.edu.ifsp.domain.model.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client {
     private String name;
     private String address;
     private CPF cpf;
+    private List<Pet> pets;
 
     public Client(String name, String address, CPF cpf) {
         this.name = name;
         this.address = address;
         this.cpf = cpf;
+        this.pets = new ArrayList<>();
     }
 
-    public CPF getCpf() {
-        return this.cpf;
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
     public void setAddress(String address) {
         this.address = address;
     }
 
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void addPet(Pet pet) {
+        this.pets.add(pet);
+    }
+
+    @Override
     public String toString() {
-        return "Cliente{" +
+        return "Client{" +
                 "name='" + name + '\'' +
                 ", address='" + address + '\'' +
-                ", cpf='" + cpf + '\'' +
+                ", cpf=" + cpf +
+                ", pets=" + pets +
                 '}';
     }
 }
