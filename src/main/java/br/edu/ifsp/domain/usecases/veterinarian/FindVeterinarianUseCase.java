73fad9c6e30a8f99dd411bedca899efcf86ae296
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-06-01 11:15:27 -0300

    feat: Implement FindVeterinarian Use Case

--- src/main/java/br/edu/ifsp/domain/usecases/veterinarian/FindVeterinarianUseCase.java
+++ src/main/java/br/edu/ifsp/domain/usecases/veterinarian/FindVeterinarianUseCase.java
@@ -1,6 +1,16 @@
 package br.edu.ifsp.domain.usecases.veterinarian;
 
-//CDU004
+import br.edu.ifsp.domain.model.user.Veterinarian;
+import br.edu.ifsp.domain.model.user.VeterinarianRepository;
 
 public class FindVeterinarianUseCase {
+    private VeterinarianRepository veterinarianRepository;
+
+    public FindVeterinarianUseCase(VeterinarianRepository veterinarianRepository) {
+        this.veterinarianRepository = veterinarianRepository;
+    }
+
+    public Veterinarian visualizarVeterinario(String crmv) {
+        return veterinarianRepository.findByCrmv(crmv);
+    }
 }
