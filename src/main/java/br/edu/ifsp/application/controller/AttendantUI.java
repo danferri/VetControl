8bd06283d787827e4c6bd3287c785e50e65acb69
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-06-11 11:34:17 -0300

    add: mais telas

---
+++ src/main/java/br/edu/ifsp/application/controller/AttendantUI.java
@@ -0,0 +1,27 @@
+package br.edu.ifsp.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class AttendantUI {
+
+    public void ManageVeterinarians(ActionEvent actionEvent) {
+    }
+
+    public void ManageClients(ActionEvent actionEvent) {
+    }
+
+    public void ManageAnimals(ActionEvent actionEvent) {
+    }
+
+    public void ManageAppointments(ActionEvent actionEvent) {
+    }
+
+    public void GenerateReports(ActionEvent actionEvent) {
+    }
+
+    public void ManagePayments(ActionEvent actionEvent) {
+    }
+
+    public void Logout(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/br/edu/ifsp/application/controller/LoginUI.java
@@ -0,0 +1,8 @@
+package br.edu.ifsp.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginUI {
+    public void Login(ActionEvent actionEvent) {
+    }
+}
