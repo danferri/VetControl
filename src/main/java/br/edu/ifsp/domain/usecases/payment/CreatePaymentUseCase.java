4b01c82c0c9747583cf1f2fa7841ecd1784e2021
GattiGabriel <ggattidev@gmail.com>
2024-06-24 21:29:17 -0300

    fix: solved merge conflicts

--- src/main/java/br/edu/ifsp/domain/usecases/payment/CreatePaymentUseCase.java
+++ src/main/java/br/edu/ifsp/domain/usecases/payment/CreatePaymentUseCase.java
@@ -3,6 +3,9 @@ package br.edu.ifsp.domain.usecases.payment;
 import br.edu.ifsp.domain.model.appointment.Appointment;
 import br.edu.ifsp.domain.model.payment.Payment;
 import br.edu.ifsp.domain.model.payment.PaymentRepository;
+import br.edu.ifsp.domain.usecases.appointment.AppointmentValidator;
+import br.edu.ifsp.domain.usecases.utils.Notification;
+import br.edu.ifsp.domain.usecases.utils.Validator;
 
 
 public class CreatePaymentUseCase {
@@ -14,6 +17,17 @@ public class CreatePaymentUseCase {
 
     public void cadastrarPagamento(Appointment appointment, String method, double amount) {
         Payment payment = new Payment(appointment, method, amount);
+
+        Validator<Payment> validatorPayment = new PaymentValidator();
+        Validator<Appointment> validatorAppointment = new AppointmentValidator();
+        Notification notificationPayment = validatorPayment.validate(payment);
+        Notification notificationAppointment = validatorAppointment.validate(appointment);
+
+        if(notificationAppointment.hasErrors())
+            throw new IllegalArgumentException(notificationAppointment.errorMessage());
+        if(notificationPayment.hasErrors())
+            throw new IllegalArgumentException(notificationPayment.errorMessage());
+
         paymentRepository.save(payment);
     }
 }
